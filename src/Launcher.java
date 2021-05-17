be567388b405e176ff305ef116c57afc86b88e8e
Lounes Bounouar <lounes.bounouar@epita.fr>
2021-05-17 15:23:44 +0200

    Partie 4 : fertig

--- src/Launcher.java
+++ src/Launcher.java
@@ -8,10 +8,34 @@ public class Launcher {
         String data = scan.nextLine().trim();
 
         while (!data.equals("quit")) {
-            System.out.println("Unknown command");
+            if (data.equals("fibo")) {
+                System.out.println("Enter n:");
+                data = scan.nextLine().trim();
+
+                int n = Integer.parseInt(data);
+
+                System.out.println(fibonacci(n));
+            }
+            else {
+                System.out.println("Unknown command");
+            }
             data = scan.nextLine().trim();
         }
         
         scan.close();
+    }
+
+    public static int fibonacci(int n) {
+        int u1 = 0;
+        int u2 = 1;
+
+        while (n > 0) {
+            int temp = u1;
+            u1 = u1 + u2;
+            u2 = temp;
+            n--;
+        }
+
+        return u1;
     }
 }
